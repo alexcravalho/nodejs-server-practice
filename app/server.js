a5f55b16544f611b6a4fe3380fadb11ae563df38
alexcravalho <a.d.cravalho@gmail.com>
2021-07-13 14:44:10 -0500

    v1.0

---
+++ app/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("I like pie");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Example app listening on port ${PORT}!"));
--- create-node-server-container.sh
+++
@@ -1,1 +0,0 @@
-docker run --name nodejs-server -d -p 5000:5000 quay.io/alexcravalho/nodeserver-prac
