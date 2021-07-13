593062accc0ec6464f626fa3ed998c01f92c418f
alexcravalho <a.d.cravalho@gmail.com>
2021-07-13 16:42:40 -0500

    fixed typo

--- app/server.js
+++ app/server.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Example app listening on port ${PORT}!"));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
